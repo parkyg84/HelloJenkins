0648b5c84d0cade662fad8cc65fc76b90b0a72d5
parky <parkyg84@naver.com>
2022-05-28 18:26:53 +0900

    write file

--- src/main/java/com/example/HelloJenkins/HelloJenkinsApplication.java
+++ src/main/java/com/example/HelloJenkins/HelloJenkinsApplication.java
@@ -3,6 +3,10 @@ package com.example.HelloJenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 @SpringBootApplication
 public class HelloJenkinsApplication {
 
@@ -11,9 +15,31 @@ public class HelloJenkinsApplication {
 
 
 
-		//
+		try {
+			System.out.println("hello");
 
-		//
+			String filePath = "D:/Source/HelloJenkins/Test.txt";
+
+			File file = new File(filePath); // File객체 생성
+			if (!file.exists()) { // 파일이 존재하지 않으면
+				file.createNewFile(); // 신규생성
+			}
+
+			// BufferedWriter 생성
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+
+			// 파일에 쓰기
+			writer.write("하이루!");
+			writer.newLine();
+			writer.write("반가워!");
+			writer.newLine();
+
+			// 버퍼 및 스트림 뒷정리
+			writer.flush(); // 버퍼의 남은 데이터를 모두 쓰기
+			writer.close(); // 스트림 종료
+		}catch (Exception ex ) {
+			ex.getMessage();
+		}
 
 
 
